6e0501873d3e1c2b84d0229dbed5fe26bcdb913b
Muhamad Ghandi Nur Setiawan <134348061+GhandiZero0X@users.noreply.github.com>
2024-12-19 00:50:09 +0700

    [+] update minor route

--- routes/route.go
+++ routes/route.go
@@ -34,37 +34,37 @@ func RouteApp(app *fiber.App) {
 	// Role routing
 	Role := app.Group("/role")
 	Role.Use(middleware.AuthMiddleware, middleware.RoleMiddleware("674039236461fc1488d67fec"))
-	Role.Post("/createRole", controllers.CreateRole) // Create a new role
-	Role.Get("/getAllRole", controllers.GetRoles) // Get all roles
-	Role.Get("/getRole/:id", controllers.GetRoleByID) // Get a role by ID
-	Role.Put("/updateRole/:id", controllers.UpdateRole) // Update a role by ID
-	Role.Delete("/deleteRole/:id", controllers.DeleteRole) // Delete a role by ID
+	Role.Post("/createRole", controllers.CreateRole) // api untuk Create a new role
+	Role.Get("/getAllRole", controllers.GetRoles) // api untuk Get all roles
+	Role.Get("/getRole/:id", controllers.GetRoleByID) // api untuk Get a role by ID
+	Role.Put("/updateRole/:id", controllers.UpdateRole) // api untuk Update a role by ID
+	Role.Delete("/deleteRole/:id", controllers.DeleteRole) // api untuk Delete a role by ID
 
 	// Modul routing
 	Modul := app.Group("/modul")
 	Modul.Use(middleware.AuthMiddleware, middleware.RoleMiddleware("674039236461fc1488d67fec"))
-	Modul.Post("/createModul", controllers.CreateModul) // Create a new modul
-	Modul.Get("/getAllModul", controllers.GetAllModul) // Get all moduls
-	Modul.Get("/getModul/:id", controllers.GetModulByID) // Get a modul by ID
-	Modul.Put("/updateModul/:id", controllers.UpdateModul) // Update a modul by ID
-	Modul.Delete("/deleteModul/:id", controllers.DeleteModul) // Delete a modul by ID
+	Modul.Post("/createModul", controllers.CreateModul) // api untuk Create a new modul
+	Modul.Get("/getAllModul", controllers.GetAllModul) // api untuk Get all moduls
+	Modul.Get("/getModul/:id", controllers.GetModulByID) // api untuk Get a modul by ID
+	Modul.Put("/updateModul/:id", controllers.UpdateModul) // api untuk Update a modul by ID
+	Modul.Delete("/deleteModul/:id", controllers.DeleteModul) // api untuk Delete a modul by ID
 
 	// Kategori routing
 	Kategori := app.Group("/kategori")
 	Kategori.Use(middleware.AuthMiddleware, middleware.RoleMiddleware("674039236461fc1488d67fec"))
-	Kategori.Post("/createKategori", controllers.CreateKategori) // Create a new kategori
-	Kategori.Get("/getAllKategori", controllers.GetAllKategori) // Get all kategoris
-	Kategori.Get("/getKategori/:id", controllers.GetKategoriByID) // Get a kategori by ID
-	Kategori.Put("/updateKategori/:id", controllers.UpdateKategori) // Update a kategori by ID
-	Kategori.Delete("/deleteKategori/:id", controllers.DeleteKategori) // Delete a kategori by ID
+	Kategori.Post("/createKategori", controllers.CreateKategori) // api untuk Create a new kategori
+	Kategori.Get("/getAllKategori", controllers.GetAllKategori) // api untuk Get all kategoris
+	Kategori.Get("/getKategori/:id", controllers.GetKategoriByID) // api untuk Get a kategori by ID
+	Kategori.Put("/updateKategori/:id", controllers.UpdateKategori) // api untuk Update a kategori by ID
+	Kategori.Delete("/deleteKategori/:id", controllers.DeleteKategori) // api untuk Delete a kategori by ID
 
 	// Jenis_user routing
 	JenisUser := app.Group("/jenis_user")
 	JenisUser.Use(middleware.AuthMiddleware, middleware.RoleMiddleware("674039236461fc1488d67fec"))
-	JenisUser.Post("/createJenisUser", controllers.CreateJenisUser) // Create a new jenis user
-	JenisUser.Get("/getAllJenisUser", controllers.GetAllJenisUser) // Get all jenis user
-	JenisUser.Get("/getJenisUser/:id", controllers.GetJenisUserByID) // Get jenis user by ID
-	JenisUser.Put("/updateTemplates/:id", controllers.UpdateTemplates) // Update templates modul jenis user
-	JenisUser.Delete("/deleteJenisUser/:id", controllers.DeleteJenisUserByID) // Delete jenis user by ID
+	JenisUser.Post("/createJenisUser", controllers.CreateJenisUser) // api untuk Create a new jenis user
+	JenisUser.Get("/getAllJenisUser", controllers.GetAllJenisUser) // api untuk Get all jenis user
+	JenisUser.Get("/getJenisUser/:id", controllers.GetJenisUserByID) // api untuk Get jenis user by ID
+	JenisUser.Put("/updateTemplates/:id", controllers.UpdateTemplates) // api untuk Update templates modul jenis user
+	JenisUser.Delete("/deleteJenisUser/:id", controllers.DeleteJenisUserByID) // api untuk Delete jenis user by ID
 
 }
